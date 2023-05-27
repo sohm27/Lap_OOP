226790cb257d0ebd65c3c3411aeb783570fb00c1
sohm27 <131216173+sohm27@users.noreply.github.com>
2023-05-27 08:26:17 +0700

    Add files via upload

---
+++ Other/Lab01/ChoosingOption.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
+public class ChoosingOption {
+    public static void main(String[] args){
+        int option=JOptionPane.showConfirmDialog(null, "Do you want to change the first class tickets?");
+        JOptionPane.showMessageDialog(null,"You've chosen: "+(option==JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    } 
+}
