ebcb27639a076298a37d91dafeb71d0f53b75afe
sohm27 <131216173+sohm27@users.noreply.github.com>
2023-06-07 23:41:32 +0700

    Create GarbageCreator.java

---
+++ Lap03/OtherProjects/hust/soict/dsai/garbage/GarbageCreator.java
@@ -0,0 +1,12 @@
+package hust.soict.dsai.garbage;
+public class GarbageCreator {
+	public static void main(String[] args) {
+		String garbage = "";
+		int i = 0;
+		while (i<1234567) {
+			garbage += i;
+			System.out.println(i);
+      i ++;
+		}
+	}
+}
