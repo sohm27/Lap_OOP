4ccc5da5b50a0a2f94fd28d3ac2c8ee5e7a4d237
sohm27 <131216173+sohm27@users.noreply.github.com>
2023-06-07 13:21:54 +0700

    Create Book.java

---
+++ Lap04/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+	private int id;
+	private String title;
+	private String category;
+	private float cost;
+	private List<String> authors=new ArrayList<String>();
+	public Book() {
+		
+	}
+}
